4071ac36c0854b11d909db859a180fab9b7ba412
Andrea Hofmann <andhofma@uni-potsdam.de>
2023-04-26 18:18:26 +0200

    new instruction selection, one run per task, plot-path

--- jnd_experiment.py
+++ jnd_experiment.py
@@ -8,13 +8,13 @@ and creates the experiment window.
 The script then iterates through the randomized tasks, executing practice sessions and trials for each task
 while providing appropriate on-screen feedback and instructions for the participant.
 
-Written by: Yana Palacheva (https://github.com/YanaPalacheva/perturbation_study/tree/main/jnd_experiment)
-Adapted by: Andrea Hofmann
+First Version by: Yana Palacheva (https://github.com/YanaPalacheva/perturbation_study/tree/main/jnd_experiment)
+Adapted Version by: Andrea Hofmann
 """
 
 
 from psychopy import core, visual, gui, data, event, prefs, monitors
-from configuration import general_experiment_configs, randomized_tasks
+from configuration import general_experiment_configs, randomized_tasks, get_task_specific_config
 from jnd_task_setup import run_jnd_task
 from pathchecks import check_config_paths
 
@@ -22,12 +22,16 @@ prefs.hardware['audioLib'] = ['PTB', 'sounddevice', 'pyo', 'pygame']
 # Check if input and output paths exist
 check_config_paths(general_experiment_configs["base_stimuli_path"],
                    general_experiment_configs["task_types"],
-                   general_experiment_configs["output_path"])  # make sure that in and out paths exist
+                   general_experiment_configs["output_path"],
+                   general_experiment_configs["plot_path"])  # make sure that in and out paths exist
 # Show initial dialog (timestamp and date)
 exp_data = {'subject': 'SUBJECT_ID', 'cur_date': data.getDateStr()}
 dlg = gui.DlgFromDict(exp_data, title=f'JND Experiment', fixed=['cur_date'])
 if not dlg.OK:
     core.quit()
+
+run = 1
+
 # Open the experiment window
 currentMonitor = monitors.Monitor(name='testMonitor')
 win = visual.Window(monitors.Monitor.getSizePix(currentMonitor),
@@ -41,22 +45,45 @@ win.flip()
 # IDE output for checking
 """print(f"task list: {randomized_tasks}")"""
 
+# Define a function to get the appropriate instruction text based on the task and ind
+def get_instruction_text(task, ind):
+    if ind == 2:
+        last_task_text = "Dies ist der letzte Teil.\n\n"
+    else:
+        last_task_text = f"Danach können Sie eine Pause machen.\n\n"
+
+    if task == 'pause':
+        return f"Sehr gut!\n" \
+               f"Wenn Sie noch Fragen haben, geben Sie der Versuchsleiterin Bescheid.\n\n" \
+               f"Es startet der {ind + 1}. von 3 Teilen.\n" \
+               f"Dieser Teil dauert zwischen 18-22 Minuten. \n" \
+               f"{last_task_text}" \
+               f"Beachten Sie: \n" \
+               f"Die Aufgabe wird nach und nach immer schwieriger,\n " \
+               f"denn die Unterschiede werden immer kleiner. \n" \
+               f"Bitte antworten Sie dennoch immer so akkurat, wie möglich.\n\n" \
+               f"Drücken Sie eine beliebige Taste sobald Sie bereit sind."
+    else:
+        return f"Sehr gut!\n" \
+               f"Wenn Sie noch Fragen haben, geben Sie der Versuchsleiterin Bescheid.\n\n" \
+               f"Es startet der {ind + 1}. von 3 Teilen.\n" \
+               f"Dieser Teil dauert zwischen 10-15 Minuten. \n" \
+               f"{last_task_text}" \
+               f"Beachten Sie: \n" \
+               f"Die Aufgabe wird nach und nach immer schwieriger,\n " \
+               f"denn die Unterschiede werden immer kleiner. \n" \
+               f"Bitte antworten Sie dennoch immer so akkurat, wie möglich.\n\n" \
+               f"Drücken Sie eine beliebige Taste sobald Sie bereit sind."
+
+
 # Iterate through randomized tasks and execute practice sessions and trials
 for ind, task in enumerate(randomized_tasks):
-    # IDE output for checking
-    """print(f"nr task: {ind}")
-    print(f"task: {task}")"""
-
     # Run practice session
     run_jnd_task(exp_data, task, win, session_type='practice')
 
-    # give some on-screen feedback?
-    visual.TextStim(win, text=f"Sehr gut!\n"
-                              f"Wenn Sie noch Fragen haben, \n geben Sie der Versuchsleiterin Bescheid.\n\n"
-                              f"Es startet der {ind+1}. von 3 Teilen.\n\n"
-                              f"Jeder der 3 Teile besteht aus 2 Blöcken.\n"
-                              f"Sie können nach jedem Block eine Pause machen.\n\n"
-                              f"Drücken Sie eine beliebige Taste sobald Sie bereit sind.",
+    # Display the appropriate instruction text based on the task
+    instruction_text = get_instruction_text(task, ind)
+    visual.TextStim(win, text=instruction_text,
                     color='black',
                     wrapWidth=2,
                     height=0.1).draw()
@@ -66,7 +93,8 @@ for ind, task in enumerate(randomized_tasks):
     win.flip()
 
     # Run the experiment
-    run_jnd_task(exp_data, task, win)
+    run_jnd_task(exp_data, task, win, run=run)
+    run += 1
 
 # Display experiment completion message
 visual.TextStim(win, text='Hervorragend, Sie haben es geschafft. \n Vielen dank! \n Drücken Sie eine beliebige Taste zum Beenden.',
