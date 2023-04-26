584c76d339c43f548b71e5cd783ad1dffa609999
Andrea Hofmann <andhofma@uni-potsdam.de>
2023-04-26 18:27:51 +0200

    shortened sentences

--- resources.py
+++ resources.py
@@ -5,21 +5,21 @@ Instruction texts
 pitch_FL_text = """
 Sie hören nachfolgend denselben Namen dreimal.
 Ein Name ist immer anders als die beiden anderen. \n
-Manchmal ist der ERSTE Name anders, als die beiden anderen - nach dem Muster ABB.
-Manchmal ist der DRITTE Name anders, als die beiden anderen - nach dem Muster BBA. \n
-Nachdem alle drei Namen vorgespielt wurden, wählen Sie bitte, welcher anders war, als die anderen beiden.
+Manchmal ist der ERSTE Name anders - nach dem Muster ABB.
+Manchmal ist der DRITTE Name anders - nach dem Muster BBA. \n
+Nachdem alle drei Namen vorgespielt wurden, wählen Sie bitte, \n welcher anders war, als die anderen beiden.
 Wenn Sie denken, der ERSTE Name ist anders, \n drücken Sie \u2190 den Pfeil nach links auf der Tastatur.
 Wenn Sie denken, der DRITTE Name ist anders, \n drücken Sie \u2192 den Pfeil nach rechts auf der Tastatur.\n
-Wenn Sie bereit sind, drücken Sie eine beliebige Taste, \n dann starten die Übungsbeispiele.
+Drücken Sie eine beliebige Taste, dann starten die Übungsbeispiele.
 """
 
 pause_text = """
 Sie hören nachfolgend dieselbe Wortgruppe dreimal. 
 Eine Wortgruppe ist immer anders als die beiden anderen. \n
-Manchmal ist die ERSTE Wortgruppe anders, als die beiden anderen - nach dem Muster ABB.
-Manchmal ist die DRITTE Wortgruppe anders, als die beiden anderen - nach dem Muster BBA. \n
-Nachdem alle drei Wortgruppen vorgespielt wurden, wählen Sie bitte, welche anders war, als die anderen beiden.
+Manchmal ist die ERSTE Wortgruppe anders - nach dem Muster ABB.
+Manchmal ist die DRITTE Wortgruppe anders - nach dem Muster BBA. \n
+Nachdem alle drei Wortgruppen vorgespielt wurden, wählen Sie bitte, \n welche anders war, als die anderen beiden.
 Wenn Sie denken, die ERSTE Wortgruppe ist anders, \n drücken Sie \u2190 den Pfeil nach links auf der Tastatur.
 Wenn Sie denken, der DRITTE Wortgruppe ist anders, \n drücken Sie \u2192 den Pfeil nach rechts auf der Tastatur.\n
-Wenn Sie bereit sind, drücken Sie eine beliebige Taste, \n dann starten die Übungsbeispiele.
+Drücken Sie eine beliebige Taste, dann starten die Übungsbeispiele.
 """
